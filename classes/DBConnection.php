6c059bbb86eaf0ed5809707ae6706d4fc9ebb0ce
Hussien Sulyman <103793784+hussiensulyman@users.noreply.github.com>
2023-12-26 21:15:11 +0200

    Update DBConnection.php

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -1,36 +1,27 @@
 <?php
-if (!defined('DB_SERVER')) {
+if(!defined('DB_SERVER')){
     require_once("../initialize.php");
 }
+class DBConnection{
 
-class DBConnection {
-    private $host;
-    private $username;
-    private $password;
-    private $database;
-    private $port;
+    private $host = DB_SERVER;
+    private $username = DB_USERNAME;
+    private $password = DB_PASSWORD;
+    private $database = DB_NAME;
+    
     public $conn;
-
+    
     public function __construct() {
-        // Set your specific MySQL URL here
-        $jawsdb_url = 'mysql://ob2izbsa8yx1yh8f:bsfxkigjebobhcbb@r98du2bxwqkq3shg.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/donadzl6ia5ws7if';
-
-        $url = parse_url($jawsdb_url);
-
-        $this->host = $url['host'];
-        $this->username = $url['user'];
-        $this->password = $url['pass'];
-        $this->database = ltrim($url['path'], '/');
-        $this->port = isset($url['port']) ? $url['port'] : 3306;
-
-        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database, $this->port);
-
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+    
         if ($this->conn->connect_error) {
+            // In a production environment, you should log this to a file instead and not output it.
             error_log("Connection failed: " . $this->conn->connect_error);
+            // For development, you could uncomment the next line to get the error message
+            // echo "Connection failed: " . $this->conn->connect_error;
             die("Connection failed. Please check the logs.");
         }
     }
-
     public function __destruct(){
         $this->conn->close();
     }
